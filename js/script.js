6fde32115f1a41295c1dabba01f42d9c9b2d0e81
hrantgartley <doyouevenjump@gmail.com>
2024-04-16 19:53:30 -0500

    working but needs styling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,58 @@
-const { map } = require("jquery");
+let nato = {
+    a: "Alpha",
+    b: "Bravo",
+    c: "Charlie",
+    d: "Delta",
+    e: "Echo",
+    f: "Foxtrot",
+    g: "Golf",
+    h: "Hotel",
+    i: "India",
+    j: "Juliet",
+    k: "Kilo",
+    l: "Lima",
+    m: "Mike",
+    n: "November",
+    o: "Oscar",
+    p: "Papa",
+    q: "Quebec",
+    r: "Romeo",
+    s: "Sierra",
+    t: "Tango",
+    u: "Uniform",
+    v: "Victor",
+    w: "Whiskey",
+    x: "Xray",
+    y: "Yankee",
+    z: "Zulu",
+};
 
-$(() => {
-  console.log("ready");
+$("#submit").on("click", () => {
+    convertToNato();
+    clearBox();
 });
 
-function convertToNato() {}
+/**
+ * Converts user input to NATO phonetic alphabet codes.
+ * @function convertToNato
+ * @params {string} input - User input.
+ * @returns {string} - NATO phonetic alphabet codes.
+ */
+function convertToNato() {
+    let input = $("#user-input").val().toLowerCase();
+    let inputArray = input.split("");
+    let natoCodes = inputArray.map((letter) => {
+        return nato[letter];
+    });
+    $("#output").text(natoCodes.join(" "));
+}
+
+/**
+ * Clears the value of the user input box.
+ * @function clearBox
+ * @params {null}
+ * @returns {null}
+ */
+function clearBox() {
+    $("#user-input").val("");
+}
