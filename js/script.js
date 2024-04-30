37da9a0f0e003f1e6d72ec97be8c92477e42b97b
hrantgartley <doyouevenjump@gmail.com>
2024-04-30 12:14:17 -0500

    final

--- js/script.js
+++ js/script.js
@@ -25,6 +25,26 @@ let nato = {
     x: "Xray",
     y: "Yankee",
     z: "Zulu",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+    0: "Zero",
+    "!": "Bang",
+    "*": "Splat",
+    "@": "At",
+    "#": "Hash",
+    "$": "Dollar",
+    "%": "Percent",
+    "^": "Hat",
+    "&": "Ampersand",
+    ",": "Comma",
+    "/": "Wack",
 };
 
 $("#submit").on("click", function () {
@@ -32,33 +52,53 @@ $("#submit").on("click", function () {
 });
 
 $("#user-input").on("input", function () {
+    printResult();
     convertToNato();
 });
 
 $("#reset").on("click", function () {
     $("#output").fadeOut(1000);
-    clearBox();
+    clearTextBox();
     clearList();
 });
 
 $("#clear").on("click", function () {
-    clearBox();
+    clearTextBox();
+});
+
+$(document).on("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        addToList();
+    }
 });
 
 /**
  * Converts user input to NATO phonetic alphabet codes.
  * @function convertToNato
- * @params {string} input - User input.
+ * @param {string} input - User input.
  * @returns {string} - NATO phonetic alphabet codes.
  */
-function convertToNato() {
-    let input = $("#user-input").val().toLowerCase();
-    let inputArray = input.split("");
+function convertToNato(input) {
+    if (typeof input !== "string" || input === null) {
+        return "";
+    }
+    let inputArray = input.toLowerCase().split("");
     let natoCodes = inputArray.map((letter) => {
         return nato[letter] || letter;
     });
+    return natoCodes.join(" ");
+}
+
+/**
+ * Prints the result of converting input to NATO phonetic alphabet codes.
+ * @function printResult
+ */
+function printResult() {
+    let input = $("#user-input").val();
+    let natoResult = convertToNato(input);
     $("#output")
-        .text("Result: " + natoCodes.join(" "))
+        .text("Result: " + natoResult)
         .fadeIn(1000);
 }
 
@@ -68,25 +108,43 @@ function convertToNato() {
  */
 function addToList() {
     let inputText = $("#user-input").val();
-    let listItem = $("<li>").addClass("list-group-item").text(inputText);
-    if (inputText === "") {
-        return;
+    if (inputText === "") return;
+    let natoText = convertToNato(inputText);
+    let listItem = $("<li>")
+        .addClass("list-group-item")
+        .text(`${inputText} in NATO: ${natoText}`);
+    if (convertToNato(inputText) > 100) {
+        listItem.addClass("text-truncate");
     }
     $("#previous-entries").append(listItem);
-    clearBox();
+    clearTextBox();
+    clearResult();
 }
 
 /**
  * Clears the value of the user input box.
  * @function clearBox
  */
-function clearBox() {
+function clearTextBox() {
     $("#user-input").val("");
 }
 
+/**
+ * Clears the list of previous entries.
+ * @function clearList
+ */
 function clearList() {
     $("#previous-entries").fadeOut(1000, function () {
         $(this).empty().fadeIn(1000);
+    });
+}
+
+/**
+ * Clears the result by fading out the output element and setting its text to an empty string.
+ */
+function clearResult() {
+    $("#output").fadeOut(1000, () => {
+        $("#output").text("");
     });
 }
 
